fb9fc1a7002de6bcd978d6367ed0ef81d0e39faf
Burr Sutter <burrsutter@gmail.com>
2015-05-25 17:58:43 +0000

    updated

--- photoresistor_poll.js
+++ photoresistor_poll.js
@@ -4,16 +4,24 @@ var photoOut = new Gpio (24,'out', 'both');
 var iv;
 
 iv = setInterval(function() {
-  console.log("start loop: " + Date.now());
+  console.log("start: " + Date.now());
   
-  reading = 0;
   photoOut.writeSync(0);
   setTimeout(function() {
-   console.log("waited: " + Date.now());
-   // while(photoIn.readSync() == 0) {
+   reading = 0;
+   console.log(" wait: " + Date.now());
+   while(photoIn.readSync() == 0) {
       reading = reading + 1;
-   //  }   	
+   } // while
+   console.log(" read: " + reading);   	
   }, .1);
-  console.log(reading);
-  console.log("end loop:" + Date.now());
-}, 250);
+ 
+  console.log("  end: " + Date.now());
+}, 500);
+
+// run this for 5 seconds, then shut it down
+setTimeout(function () {
+  console.log("All Finished, shutting down");
+  clearInterval(iv); // shutdown
+  photoOut.unexport();    // Unexport GPIO and free resources
+}, 5000);
